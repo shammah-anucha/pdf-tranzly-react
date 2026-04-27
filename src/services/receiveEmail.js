d62296b91f90ecc78fe929144f10870f0d83c485
Shammah Anucha <shammah.anucha@yahoo.com>
2026-04-28 01:08:00 +0200

    vite

--- src/services/receiveEmail.js
+++ src/services/receiveEmail.js
@@ -47,8 +47,27 @@
 //   return true;
 // }
 
+// export async function receiveEmail({ name, email, message }) {
+//   const response = await fetch("http://localhost:5050/api/send-email", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ name, email, message }),
+//   });
+
+//   if (!response.ok) {
+//     const text = await response.text();
+//     throw new Error(`Email send failed: ${response.status} ${text}`);
+//   }
+
+//   return true;
+// }
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function receiveEmail({ name, email, message }) {
-  const response = await fetch("http://localhost:5050/api/send-email", {
+  const response = await fetch(`${API_URL}/api/send-email`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
